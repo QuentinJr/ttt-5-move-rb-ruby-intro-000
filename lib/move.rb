95395c724c7b87264ebaaa773ebbdae4b36430d6
Quentin Wilkerson Jr <quentin.a.wilkerson@gmail.com>
2020-08-19 02:38:07 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,8 +7,7 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
-def input_to_index()
-  user_move = gets.strip
+def input_to_index(user_move)
   board[user_move - 1] = user_move
 end
 
